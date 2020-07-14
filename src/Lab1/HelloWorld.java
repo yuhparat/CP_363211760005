9fd5c346df6501a8379d96be925817eb6df31261
bell <yuhparat.l@rmutsvmail.com>
2020-07-14 17:39:57 +0700

    Initial commit

---
+++ src/Lab1/HelloWorld.java
@@ -0,0 +1,12 @@
+package Lab1;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World !!!");
+        System.out.println("yuhparat");
+
+    }
+
+
+}

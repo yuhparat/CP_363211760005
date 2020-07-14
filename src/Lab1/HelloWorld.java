29066b3c8a81e07a20ec15382d3f89e1cf0f741f
bell <yuhparat.l@rmutsvmail.com>
2020-07-14 17:53:30 +0700

    add code 3 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("yuhparat");
+        System.out.println("มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย");
+        System.out.println("คณะการจัดการ");
+        System.out.println("สาขาเทคโนโลยีสารสนเทศ");
 
     }
 

1376c9fa6276b36be5476490520cff33d87332aa
LabCom_MT-4 <yuhprat.l@rmutsvmail.com>
2020-09-29 15:52:12 +0700

    lad 3

---
+++ src/Lab5/ArraySorting.java
@@ -0,0 +1,14 @@
+package Lab5;
+//sorting คือการเรียงลำดับข้อมูล
+public class ArraySorting {
+    public static void main(String[] args){
+
+        Integer number[]= {12,54,87,32,69};
+
+        System.out.println(ArraySorting);
+
+
+        }
+
+    }
+
